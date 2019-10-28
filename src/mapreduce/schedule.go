83a2947296de450fa18c194dd14ef190c2f321f0
wlgq2 <orcaer@yeah.net>
2019-10-28 19:47:48 +0800

    mapreduce partIII

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,10 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -22,13 +26,38 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 		ntasks = nReduce
 		n_other = len(mapFiles)
 	}
-
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
-	// All ntasks tasks have to be scheduled on workers. Once all tasks
-	// have completed successfully, schedule() should return.
-	//
-	// Your code here (Part III, Part IV).
-	//
+	//设置等待任务数量
+	var wg sync.WaitGroup
+	wg.Add(ntasks)
+	//运行协程并行执行任务
+	for i := 0; i < ntasks; i++ {
+		go func(index int) {
+			//rpc调用参数
+			var args DoTaskArgs
+			args.Phase = phase
+			args.JobName = jobName
+			args.NumOtherPhase = n_other
+			args.TaskNumber = index
+			if phase == mapPhase {
+				args.File = mapFiles[index]
+			}
+
+			worker := <-registerChan
+			for {
+				rst := call(worker, "Worker.DoTask", args, nil)
+				if rst {
+					//任务完成
+					wg.Done()
+					registerChan <- worker
+					break
+				}
+				time.Sleep(time.Duration(10) * time.Millisecond)
+			}
+		}(i)
+	}
+	//等待所有任务完成
+	wg.Wait()
 	fmt.Printf("Schedule: %v done\n", phase)
 }
