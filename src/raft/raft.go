23f388023ac838595ae3a488b91503bf8914e185
wlgq2 <orcaer@yeah.net>
2019-11-16 22:59:30 +0800

    ...

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -569,7 +569,6 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 }
 
 func (rf *Raft) Kill() {
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05.000"), rf.me, "kill")
 	close(rf.killChan)
 }
 
